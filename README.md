f327b66275fbb4f9ab1fc7979eefceb8c7ef9d88
ydnafir <rifandyredz@gmail.com>
2023-01-03 23:33:32 +0700

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# SARIP (Sistem Akademik Rekayasa IP)
+
+Chrome Extension auto IP 4
+
+# Preview
+
+![image](https://user-images.githubusercontent.com/87485052/210394315-a9a83cbe-15e0-45d8-8432-4e86b7d3b50a.png)
+
+# Instalation
+
+1. Download [ZIP file](https://github.com/ydnafir/sarip/archive/refs/heads/master.zip)
+2. unzip di suatu folder.
+3. Buka [extensions](chrome://extensions) di Chrome/edge/dll.
+4. Nyalain Developer mode (pojok kanan atas kalo ada, kalo gaada googling aja).
+5. Click "Load Unpacked".
+6. Pilih folder yg sesuai sama folder di no 2.
+7. Buka status mahasiswa
+8. Be responsible! :)
---
+++ content-script.js
@@ -0,0 +1,28 @@
+const main = () => {
+  // Ganti NR
+  let dataMahasiswa = document.getElementsByClassName('table-striped')[0]
+  let listData = dataMahasiswa.querySelectorAll('tbody tr')
+  // listData[7]ip ipk
+  let ipIPK = listData[7].querySelectorAll('td span')
+  ipIPK.forEach((ip) => {
+    ip.textContent = '4.00'
+  })
+  // ip TPB
+  listData[9].querySelector('td').textContent = '4.00 / Lulus 36 SKS'
+  //  NR
+  listData[10].querySelector('td').textContent = '4.00 / 24 SKS'
+
+  // Ganti index di tabel
+  let tabelIndex = document.getElementsByClassName('table-striped')[1]
+  let rowIndex = tabelIndex.querySelectorAll('table tr')
+
+  rowIndex.forEach((row) => {
+    try {
+      let tdElements = row.querySelectorAll('td')
+      tdElements[5].textContent = 'A'
+    } catch (err) {}
+  })
+
+  console.log('ja;an')
+}
+main()
